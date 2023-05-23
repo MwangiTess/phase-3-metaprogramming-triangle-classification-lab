f8d2535bd6ea27d028bf94f8df4d021303ad41fd
Teresia <teresiamwangi26@gmail.com>
2023-05-23 21:17:20 +0300

    Complete ASsignment

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,36 @@
 class Triangle
-  # write code here
+  class TriangleError < StandardError
+  end
+
+  def initialize(length, width, height)
+    @length = length
+    @width = width
+    @height = height
+  end
+
+  def kind
+    validate_triangle
+
+    if @length == @width && @width == @height
+      :equilateral
+    elsif @length == @width || @length == @height || @width == @height
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  private
+
+  def validate_triangle
+    sides = [@length, @width, @height].sort
+    if sides.min <= 0 || sides[0] + sides[1] <= sides[2]
+      raise TriangleError, 'Invalid triangle'
+    end
+  end
 end
+
+triangle = Triangle.new(4, 4, 3)
+triangle_kind = triangle.kind
+
+puts triangle_kind
